ed00b80923d3aec087e23a72e06b2b8d49f9c54b
RuliSlim <ruli0to8@gmail.com>
2021-01-15 22:29:20 +0700

    table ok sangat

--- src/components/table/table.js
+++ src/components/table/table.js
@@ -1,6 +1,5 @@
 import React from "react";
 import PropTypes from "prop-types";
-import Modal from "../modal/modal";
 import MyButton from "../atoms/button";
 import { useDispatch } from "react-redux";
 import { closeModal, openModal } from "../../store/modal/action";
@@ -23,8 +22,8 @@ function CreateTitle({ data }) {
 
 	return (
 		<thead className="w-full text-center">
-			<tr className={`lg:grid ${col} w-full bg-gray-300`}>
-				{key.map((el, i) => <th className={`${i === 0 ? "border-l-2" : ""} ${i === key.length - 2 || i === key.length-3 || i === 1 ? "col-span-2" : ""} border-b-2 py-4 border-t-2 border-r-2 border-black border-opacity-10`} key={el}>{el}</th>)}
+			<tr className={`lg:grid ${col} w-full bg-gray-400`}>
+				{key.map((el, i) => <th className={`${i === 0 ? "border-l-4" : ""} ${i === key.length - 2 || i === key.length-3 || i === 1 ? "col-span-2" : ""}  py-4 border-t-4 border-r-4 border-black border-opacity-100`} key={el}>{el}</th>)}
 			</tr>
 		</thead>
 	);
@@ -55,9 +54,9 @@ function CreateBody({ data, isFilter }) {
 		dispatch(filterFilms(e.target.value, type));
 	};
 
-	const descRow = (val, i) => {
+	const descRow = (val, i, id) => {
 		return (
-			<td key={val + i} className={`${i === 0 ? "border-l-2" : ""}  ${i === key.length - 2 || i === key.length - 3  || i === 1 ? "col-span-2" : ""} max-w-md max-h-xs truncate border-b-2 border-r-2 border-black border-opacity-10 py-2`}>
+			<td key={val + i} className={`${i === 0 ? "border-l-4" : ""} ${id === data.length - 1 ? "border-b-4" : ""}  ${i === key.length - 2 || i === key.length - 3  || i === 1 ? "col-span-2" : ""} max-w-md max-h-xs truncate  border-r-4 border-black border-opacity-100 py-2`}>
 				<div className="float-right">
 					<MyButton handleClick={handleModal(val)} type="iconTable" text={<i className="ri-information-fill text-xl"></i>}/>
 				</div>
@@ -66,17 +65,17 @@ function CreateBody({ data, isFilter }) {
 		);
 	};
 
-	const normalRow = (val, i) => {
+	const normalRow = (val, i, id) => {
 		return (
-			<td key={val + i} className={`${i === 0 ? "border-l-2" : ""}  ${i === key.length - 2 || i === key.length - 3 || i === 1 ? "col-span-2" : ""} max-w-md max-h-xs truncate border-b-2 border-r-2 border-black border-opacity-10 py-2`}>
+			<td key={val + i} className={`${i === 0 ? "border-l-4" : ""} ${id === data.length - 1 ? "border-b-4" : ""} ${i === key.length - 2 || i === key.length - 3 || i === 1 ? "col-span-2" : ""} max-w-md max-h-xs truncate  border-r-4 border-black border-opacity-100 py-2`}>
 				<p>{val}</p>
 			</td>
 		);
 	};
 
-	const actionRow = (val, i) => {
+	const actionRow = (val, i, id) => {
 		return (
-			<td td key={val} className={`${i === 0 ? "border-l-2" : ""} ${i === key.length - 2 || i === key.length - 3  || i === 1 ? "col-span-2" : ""} max-w-md max-h-xs truncate border-b-2 border-r-2 border-black border-opacity-10 px-4 py-2`}>
+			<td td key={val} className={`${i === 0 ? "border-l-2" : ""} ${id === data.length - 1 ? "border-b-2" : ""} ${i === key.length - 2 || i === key.length - 3  || i === 1 ? "col-span-2" : ""} max-w-md max-h-xs truncate  border-r-4 border-black border-opacity-100 px-4 py-2`}>
 				<CreateOption state={false} />
 			</td>
 		);
@@ -84,7 +83,7 @@ function CreateBody({ data, isFilter }) {
 
 	const filterRow = (val, i, id) => {
 		return (
-			<td key={val} className={`${i === 0 ? "border-l-2" : ""}  ${i === key.length - 2 || i === key.length - 3 || i === 1 ? "col-span-2" : ""} max-w-md max-h-xs truncate border-b-2 border-r-2 border-black border-opacity-10 py-2`}>
+			<td key={val} className={`${i === 0 ? "border-l-4" : ""} ${id === data.length - 1 ? "border-b-4" : ""}  ${i === key.length - 2 || i === key.length - 3 || i === 1 ? "col-span-2" : ""} max-w-md max-h-xs truncate  border-r-4 border-black border-opacity-100 py-2`}>
 				{
 					id === 0 && (i === 1 || i === 3)
 						? <input className="border-2 border-black" onChange={handleFilter(i === 1 ? "film" : "genre")}/>
@@ -103,23 +102,23 @@ function CreateBody({ data, isFilter }) {
 							?
 							(i === key.length - 1 && id !== 0)
 								?
-								actionRow(val, i)
+								actionRow(val, i, id)
 								:
 								(i === 4 && id !== 0)
 									?
-									descRow(val, i)
+									descRow(val, i, id)
 									:
 									filterRow(val, i, id)
 							:
 							(i === key.length - 1)
 								?
-								actionRow(val, i)
+								actionRow(val, i, id)
 								:
 								(i === 4)
 									?
-									descRow(val, i)
+									descRow(val, i, id)
 									:
-									normalRow(val, i)
+									normalRow(val, i, id)
 					))}
 				</tr>
 			))}
